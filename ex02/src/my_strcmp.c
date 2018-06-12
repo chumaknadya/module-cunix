a978e0b9dfba568105dc3fcf4f4063f334309dc0
chumaknadya <chumaknadya19@gmail.com>
2018-06-12 13:14:19 +0300

    modified 2 task

--- ex02/src/my_strcmp.c
+++ ex02/src/my_strcmp.c
@@ -1,6 +1,6 @@
 int my_strcmp(char *s1, char *s2) {
-    const unsigned char *p1 = (const unsigned char *)s1;
-    const unsigned char *p2 = (const unsigned char *)s2;
+    const char *p1 = (const char *)s1;
+    const char *p2 = (const char *)s2;
 
     while (*p1 != '\0') {
         if (*p2 == '\0') return  1;
