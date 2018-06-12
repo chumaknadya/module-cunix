c80a21878d5ae6b44aee417e74e302f5a6d32e34
chumaknadya <chumaknadya19@gmail.com>
2018-06-12 16:10:52 +0300

    done 9 task

--- ex09/src/hash.c
+++ ex09/src/hash.c
@@ -2,13 +2,12 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "linked_list.h"
-hashtable_t     *hash_create(unsigned int size){
+
+hashtable_t *hash_create(unsigned int size){
   hashtable_t *new_table;
   if (size < 1)
     return NULL;
-  if ((new_table = malloc(sizeof(hashtable_t))) == NULL) {
-       return NULL;
-  }
+  new_table = malloc(sizeof(hashtable_t));
   new_table->table = malloc(size*sizeof(node_t*));
   for(int i = 0; i < size; i++)
      new_table->table[i] = NULL;
@@ -17,9 +16,9 @@ hashtable_t     *hash_create(unsigned int size){
 
   return new_table;
 }
-void            hash_destroy(hashtable_t *ht, void (*fp)(void *data)){
-  for(int i = 0; i < ht->size;i++){
-    if( ht->table[i] != NULL){
+void hash_destroy(hashtable_t *ht, void (*fp)(void *data)){
+  for(int i = 0; i < ht->size;i++) {
+    if( ht->table[i] != NULL) {
       node_t  *ptr = ht->table[i];
       list_destroy(&(ptr),fp);
     }
@@ -28,7 +27,7 @@ void            hash_destroy(hashtable_t *ht, void (*fp)(void *data)){
   free(ht);
 
 }
-unsigned int    hash_func(char *key){
+unsigned int hash_func(char *key){
   if ( key == NULL)
      return 0;
   unsigned int res = 0;
@@ -39,7 +38,7 @@ unsigned int    hash_func(char *key){
   return res;
 }
 
-void            hash_set(hashtable_t *ht, char *key, void *ptr, void (*fp)(void *data)){
+void hash_set(hashtable_t *ht, char *key, void *ptr, void (*fp)(void *data)){
     int index = hash_func(key)%ht->size;
     fp(ptr);
     if ( ht->table[index] == NULL)
@@ -49,7 +48,7 @@ void            hash_set(hashtable_t *ht, char *key, void *ptr, void (*fp)(void 
 
 
 }
-void            *hash_get(hashtable_t *ht, char *key){
+void *hash_get(hashtable_t *ht, char *key){
     int index = hash_func(key)%ht->size;
     if (ht->table[index] == NULL)
       return NULL;
